70d364adfe71c76d3db5d52475ed33cca5f9cade
reshmabidikar <reshmabidikar@outlook.com>
2023-08-11 14:03:06 +0530

    search tenant key by prefix added

--- tenant/tenant-key-value.py
+++ tenant/tenant-key-value.py
@@ -19,8 +19,15 @@ print("retrieve")
 key_name = 'demo_key_python'
 tenantKeyValue = tenantApi.get_user_key_value(key_name)
 print(tenantKeyValue)
+#
+# print("delete")
+# tenantApi = killbill.api.TenantApi()
+# key_name = 'demo_key_python'
+# tenantApi.delete_user_key_value(key_name, created_by='demo')
 
-print("delete")
+print("Retrieve based on key prefix")
 tenantApi = killbill.api.TenantApi()
-key_name = 'demo_key_python'
-tenantApi.delete_user_key_value(key_name, created_by='demo')
+
+tenantKeyValues = tenantApi.get_all_plugin_configuration(key_prefix='demo_key')
+print(tenantKeyValues)
+
